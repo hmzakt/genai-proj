9ffbe9a26281f50e33602916f737516ac166ca6e
hmz akt <hmzakt11@gmail.com>
2026-02-03 21:59:13 +0530

    deployment error

--- backend/src/services/resumeProcess.service.js
+++ backend/src/services/resumeProcess.service.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
-const py_url = "http://localhost:8000/process-resume";
+const AI_SERVICE_BASE_URL = process.env.AI_SERVICE_URL || "http://localhost:8000";
+const AI_PROCESS_PATH = "/process-resume";
+const aiProcessUrl = new URL(AI_PROCESS_PATH, AI_SERVICE_BASE_URL).toString();
 
 async function processResume(resumeUrl, jobDescription){
-    const response = await axios.post(py_url,{
+    const response = await axios.post(aiProcessUrl,{
         resume_url: resumeUrl,
         job_description:jobDescription
     });
